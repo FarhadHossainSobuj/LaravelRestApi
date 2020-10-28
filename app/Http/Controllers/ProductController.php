7302bf2b28ad8128fff6fc6806d60bdfa0890ee4
FarhadHossainSobuj <tlcfarhad@gmail.com>
2020-10-28 09:00:27 +0600

    product destroy

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -99,6 +99,9 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return \response([
+            'data'=>"Product successfylly deleted"
+        ], Response::HTTP_NO_CONTENT);
     }
 }
